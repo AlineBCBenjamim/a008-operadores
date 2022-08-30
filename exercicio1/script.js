961ba830c8a79769eb10de8a53c0060b77ecddc9
Aline Carvalho <alineoak9@gmail.com>
2022-08-30 18:27:10 -0300

    Resolução de exercícios

---
+++ exercicio1/script.js
@@ -0,0 +1,14 @@
+//a.
+
+const numero = prompt("Insira um número par");
+
+//b.
+console.log(numero%2);
+
+//c.
+// Existe um padrão Sim, colocando numero par o resto da divisão será 0.
+
+//d.
+// O ímpar o resto da divisão sempre será 1.
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,16 @@
+// a.
+const alternativaA = 5 > 20 && 5 < 2;
+console.log("Alternativa a é:", alternativaA);
+
+//b.
+const alternativaB = 5 === 5 || 5 === "5";
+console.log("Alternativa b é:", alternativaB);
+
+//c.
+const alternativaC = !(20 > 50);
+console.log("Alternativa c é", alternativaC);
+
+//d.
+
+const alternativaD = !(20 > 50 || 50 > 60);
+console.log("Alternativa d é", alternativaD);
